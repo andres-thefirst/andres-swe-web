00ac82467579084bed433694af7ba78451a600eb
andres-thefirst <darkshadowskull@gmail.com>
2020-10-11 20:56:16 -0700

    Removes font awesome

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
         <meta name="robots" content="index, follow" />
 		    <meta name="revisit-after" content="14 days" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
     </Head>
     <style jsx>
     {`
